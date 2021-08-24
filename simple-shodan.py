7681d92f6790c4cb26aa612f2c630a9c05ec944b
Shell Reaper <76930483+shellreaper@users.noreply.github.com>
2021-08-25 00:18:28 +0200

    Update simple-shodan.py

--- simple-shodan.py
+++ simple-shodan.py
@@ -1,24 +1,33 @@
-#!/#!/usr/bin/python3
-
 import shodan
 import sys
-
-API_KEY = 'Your_api_key_here'
+from multiprocessing import Process
+API_KEY = 'h9qw0xeTqxSb4myUzrlre0Ivf68JnsL8'
 if len(sys.argv) == 1:
-    print('example : python3 simple-shodan.py apache')
-    sys.exit()
-try:
-        #Setting up the api key
-        api = shodan.Shodan(API_KEY)
+	print('example : python3 simple-shodan.py apache')
+	sys.exit()
 
-        #Searching Phase 
-        query = sys.argv[1]
-        result = api.search(query)
-
-        #locating the matching IP addresses
-        for service in result['matches']:
-                print(service['ip_str'])
-except Exception as e:
-        print('Error: {}'.format(e))
-        sys.exit(1)
-
+def main():
+	try:
+		api = shodan.Shodan(API_KEY)
+		query = sys.argv[1]
+		result = api.search(query)
+		for service in result['matches']:
+			print(service['ip_str'])
+			print('listening for certs...')
+			def ssl():
+				
+				for banner in api.stream.ports([443, 8443]):
+					try:						
+						if 'ssl' in banner:
+							print(banner['ssl'])
+					except Exception as i:
+						print("couldn't retrieve any certs")
+						continue
+			action_process = Process(target=ssl)
+			action_process.start()
+			action_process.join(timeout=5)
+			action_process.terminate()
+			print('Timed out .....')
+	except Exception as e:
+		print('Error {}: '.format(e))
+main()
